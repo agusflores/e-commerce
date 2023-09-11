6d3bc5fa68441d05935fa0932ebf85acd28cb703
Agustin Flores <agustinflores1505@gmail.com>
2023-09-11 15:34:32 -0300

    fix texto

--- src/App.jsx
+++ src/App.jsx
@@ -1031,7 +1031,7 @@ function App() {
         <div className="flex flex-col bg-white rounded-xl dark:bg-slate-900">
           <div className="flex-auto p-4 md:p-6">
             <p className="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
-              " With Preline, we're able to easily track our performance in full detail. It's become an essential tool for us to grow and engage with our audience. "
+              With Preline, we are able to easily track our performance in full detail. Its become an essential tool for us to grow and engage with our audience. 
             </p>
           </div>
 
@@ -1058,7 +1058,7 @@ function App() {
         <div className="flex flex-col bg-white rounded-xl dark:bg-slate-900">
           <div className="flex-auto p-4 md:p-6">
             <p className="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
-              " In September, I will be using this theme for 2 years. I went through multiple updates and changes and I'm very glad to see the consistency and effort made by the team. "
+              In September, I will be using this theme for 2 years. I went through multiple updates and changes and Im very glad to see the consistency and effort made by the team. 
             </p>
           </div>
 
@@ -1084,7 +1084,7 @@ function App() {
         <div className="flex flex-col bg-white rounded-xl dark:bg-slate-900">
           <div className="flex-auto p-4 md:p-6">
             <p className="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
-              " Refreshing and Thought provoking design and it changes my view about how I design the websites. Great typography, modern clean white design, nice tones of the color. "
+              Refreshing and Thought provoking design and it changes my view about how I design the websites. Great typography, modern clean white design, nice tones of the color. 
             </p>
           </div>
 
@@ -1161,7 +1161,7 @@ function App() {
       <div className="mt-3 grid space-y-3 text-sm">
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">About us</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Blog</a></p>
-        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Careers</a> <span className="inline text-blue-600 dark:text-blue-500">— We're hiring</span></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Careers</a> <span className="inline text-blue-600 dark:text-blue-500">— We are hiring</span></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Customers</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Newsroom</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Sitemap</a></p>
@@ -1175,7 +1175,7 @@ function App() {
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Community</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Help & Support</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">eBook</a></p>
-        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">What's New</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Whats New</a></p>
         <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Status</a></p>
       </div>
     </div>
