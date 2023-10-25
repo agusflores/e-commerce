1fdd23b539beaa9d5a4ee5e8f9310d8368c99767
Agustin Flores <agustinflores1505@gmail.com>
2023-10-25 19:06:23 -0300

    alert al realizar compra

---
+++ src/components/atoms/Alert.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom'
+
+export const Alert = ({ onClose }) => {
+  return (
+    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <h3 className="text-base font-semibold leading-6 text-gray-900">
+                  Compra terminada
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    La compra se ha realizado con exito! Muchas gracias por
+                    comprar en nuestra tienda online
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 justify-center align-center  sm:flex sm:flex-row-reverse sm:px-6">
+            <Link to="/home">
+              <button
+                onClick={onClose}
+                type="button"
+                className="mt-3 inline-flex w-full rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                Cancel
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/molecules/FormDetailPurchase.jsx
+++ src/components/molecules/FormDetailPurchase.jsx
@@ -1,16 +1,18 @@
 import { TextField } from '@mui/material'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CartContext } from '../../context/CartProvider'
-import { Link } from 'react-router-dom'
+import { Alert } from '../atoms/Alert'
 
 export const FormDetailPurchase = () => {
   const cartContext = useContext(CartContext)
+  const [open, setOpen] = useState(false)
 
   const handleFinishPurchase = () => {
     cartContext.setCart([])
     cartContext.setQuantity(0)
     cartContext.setTotal(0)
   }
+
   return (
     <>
       <h1 className="font-bold text-black-500 text-2xl">Checkout</h1>
@@ -35,15 +37,18 @@ export const FormDetailPurchase = () => {
           label="Direccion"
           variant="outlined"
         />
-        <Link to="/home" aria-current="page">
+        <div>
           <button
-            onClick={handleFinishPurchase}
-            type="submit"
+            onClick={() => {
+              setOpen(true)
+            }}
+            type="button"
             className="w-2/6 mt-10 rounded-md bg-blue-500 py-2 px-2 font-medium text-blue-50 hover:bg-blue-600"
           >
             Confirmar compra
           </button>
-        </Link>
+          {open && <Alert onClose={() => handleFinishPurchase()} />}
+        </div>
       </form>
     </>
   )
